a3e3ccb06e061ce3f4c17f00f346e8594324fbed
David Lipsky <dlipsky@gmail.com>
2022-05-10 20:26:28 +0700

    moved createRouterContext into Router.js

--- src/lib/redux-saga-router/router/index.js
+++ src/lib/redux-saga-router/router/index.js
@@ -2,5 +2,4 @@ import React from 'react'
 
 export const RouterContext = React.createContext({})
 
-export { default as createRouterContext } from './createRouterContext'
 export { default as prepareRoutes } from './prepareRoutes'
